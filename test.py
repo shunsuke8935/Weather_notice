1ad0d73e5721fd1d09d01b014e0ad4fb210f76e9
shunsu <shunsuke8935@gmail.com>
2022-06-30 11:11:59 +0900

    fix bug

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import requests
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome import service as fs
+import chromedriver_binary
+import json
+import chardet
+# -*- coding: utf-8 -*-
 
 
 def Line_Message(message):
@@ -25,76 +30,37 @@ def Line_Message(message):
 
 
 def Selenium():
+    # 検証
+    print("START")
+    driver_path = '/app/.chromedriver/bin/chromedriver'
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--lang=ja-JP')
+    # driverに設定 ※optionsを指定しないとheadlessにならないので注意
+    driver = webdriver.Chrome(options=options, executable_path=driver_path)
+
+    # chrome_service = fs.Service(
+    #     executable_path='/Users/shunsukegoto/Desktop/DEVELOP/Weather_notice/chromedriver')
+
+    # 気温取得
     try:
-        #検証
-        print("START")
-        driver_path = '/app/.chromedriver/bin/chromedriver'
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--lang=ja-JP')
-        #driverに設定 ※optionsを指定しないとheadlessにならないので注意
-        # driver = webdriver.Chrome(options=options, executable_path=driver_path)
 
-        # 開発用
-        driver = webdriver.Chrome(
-            '/Users/shunsukegoto/Desktop/DEVELOP/Weather_notice/chromedriver',
-            
-        )
-
-        #お天気サイト
-        driver.get('https://tenki.jp/')
-        if not 'https://tenki.jp/' in driver.current_url:
-            raise Exception
-    except:
-        driver.quit()
-        return "SELENIUM ERROR CAN NOT ACCESS TO https://tenki.jp/"
-
-    #対象地域検索
-    try:
-        print("SUCCESS ACCESS TO https://tenki.jp/")
-        search_box = driver.find_element_by_id("keyword")
-        time.sleep(5)
-        search_box.click()
-        time.sleep(5)
-        search_box.send_keys('京田辺市')
-        search_box.submit()
-    except:
-        driver.quit()
-        return "CAN NOT SEARCH ON https://tenki.jp/"
-
-    #検索結果の候補から選択
-    try:
-        kyotanabeshi = driver.find_element_by_xpath(
-            '/html/body/div[2]/section/section[1]/div[2]/p[1]/a'
-        )
-        time.sleep(5)
-        kyotanabeshi.click()
-    except:
-        driver.quit()
-        return "CAN NOT CLICK SEARCH RESULT"
-
-    #気温取得
-    try:
-        time.sleep(30)
-        #最高気温取得
-        temparcher = driver.find_element_by_xpath(
-            '/html/body/div[3]/section/div[3]/section[2]/div[1]/div[2]/dl/dd[1]/span[1]'
-        ).text
-        #明日の天気情報
-        weather_info = driver.find_element_by_xpath("/html/body/div[3]/section/div[3]/section[2]/div[1]/div[1]/p").text
-        #最低気温
-        temparcher_low = driver.find_element_by_xpath("/html/body/div[3]/section/div[3]/section[2]/div[1]/div[2]/dl/dd[3]/span[1]").text
-        #午前中の降水確率
-        rainy_parcent_morning = driver.find_element_by_xpath("/html/body/div[3]/section/div[3]/section[2]/div[2]/table/tbody/tr[2]/td[2]").text
-        #午後の降水確率
-        rainy_parcent_after = driver.find_element_by_xpath("/html/body/div[3]/section/div[3]/section[2]/div[2]/table/tbody/tr[2]/td[3]").text
-    except:
-        driver.quit()
-        return "CAN NOT GET TEMPARCHER"
-
-    #google検索画面へ
-    try:
+        url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json'
+        r = requests.get(url)
+        res_json = json.loads(r.text)
+        print(res_json)
+        weather_info = res_json[0]["timeSeries"][0]["areas"][0]["weathers"][1]
+        temparcher_low = res_json[1]["timeSeries"][1]["areas"][0]["tempsMin"][1]
+        temparcher = res_json[1]["timeSeries"][1]["areas"][0]["tempsMax"][1]
+        table = str.maketrans({
+            '\u3000': '',
+            ' ': '',
+            '\t': ''
+        })
+        weather_info = weather_info.translate(table)
+        driver = webdriver.Chrome()
         print("気温: {temparcher}".format(temparcher=temparcher))
+
         driver.get('https://www.google.com/')
         search_box = driver.find_element_by_name("q")
         search_box.click()
@@ -103,22 +69,20 @@ def Selenium():
         search_box.submit()
         result = driver.find_elements_by_css_selector("div.tF2Cxc a")
         result_link = result[0]
-        
-        time.sleep(10)
+
+        time.sleep(5)
         result_link.click()
-        time.sleep(10)
+        time.sleep(5)
         url = driver.current_url
 
         result_str = """
-        \n明日の天気：{weather_info}\n最高気温：{temparcher}\n最低気温：{temparcher_low}\n午前中の降水確率：{rainy_parcent_morning}\n午後の降水確率：{rainy_parcent_after}\nお天気ファッション情報：\n{url}
+        \n明日の天気：{weather_info}\n最高気温：{temparcher}\n最低気温：{temparcher_low}\nお天気ファッション情報：\n{url}
         """.format(
             weather_info=weather_info,
             temparcher=temparcher,
             temparcher_low=temparcher_low,
-            rainy_parcent_morning=rainy_parcent_morning,
-            rainy_parcent_after=rainy_parcent_after,
             url=url
-            )
+        )
 
         return result_str
     except:
@@ -133,14 +97,5 @@ def main():
     result_url = Selenium()
     Line_Message(result_url)
 
+
 main()
-
-
-# schedule.every().day.at("21:22").do(main())
-
-# # タスク監視ループ
-# while True:
-#     # 当該時間にタスクがあれば実行
-#     schedule.run_pending()
-#     # 1秒スリープ
-#     time.sleep(100)
